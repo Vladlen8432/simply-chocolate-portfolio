a0c8e18152006e450e1f0862214a57919fbb13cc
Vladlen8432 <127045075+Vladlen8432@users.noreply.github.com>
2024-04-06 17:19:20 +0300

    checkpoint

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 "use strict";
 
+/* ========== Open/Close button ========== */
+
 document.addEventListener("DOMContentLoaded", function () {
   const modal = document.querySelector(".modal");
   const modalTrigger = document.querySelector(".menu-header");
@@ -15,4 +17,32 @@ document.addEventListener("DOMContentLoaded", function () {
 
   modalTrigger.addEventListener("click", openModal);
   closeButton.addEventListener("click", closeModal);
+});
+
+/* ========== Play video ========== */
+
+document.addEventListener("DOMContentLoaded", function () {
+  const buttonVideo = document.querySelector(".button-video");
+
+  buttonVideo.addEventListener("click", function () {
+    const container = buttonVideo.closest(".container-preview-video");
+    const pictureElement = container.querySelector("picture");
+
+    pictureElement.remove();
+
+    const iframeElement = document.createElement("iframe");
+    iframeElement.src =
+      "https://www.youtube.com/embed/KEqpwpfFfhE?si=MhbrZOQsdM2LRDGK&autoplay=1";
+
+    iframeElement.title = "YouTube video player";
+    iframeElement.frameborder = "0";
+    iframeElement.allow =
+      "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share";
+
+    iframeElement.allowfullscreen = true;
+
+    container.appendChild(iframeElement);
+
+    buttonVideo.style.display = "none";
+  });
 });
